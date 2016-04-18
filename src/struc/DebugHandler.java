0c32b8dfd8a2e59db00e718cac7727ffad9e0341
Spence Southard <Spence.Southard@gmail.com>
2016-04-18 13:58:08 -0400

    More styling updates

--- src/struc/DebugHandler.java
+++ src/struc/DebugHandler.java
@@ -4,21 +4,21 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 
-public class DebugHandler{
+public class DebugHandler {
 
     public static void appendln(String filename, String textVal){
-        append(filename, textVal+'\n');
+        append(filename, textVal + '\n');
     }
 
-    public static void append(String filename, String textVal){
+    public static void append(String filename, String textVal) {
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         try {
             BufferedWriter out = new BufferedWriter(new FileWriter(filename , true));
             out.write(timeStamp + " : " + textVal);
             out.close();
         }
-        catch (IOException e) {
-            System.out.println("exception in write "+ e);
+        catch(IOException e) {
+            System.out.println("exception in write " + e);
         }
     }
 }
